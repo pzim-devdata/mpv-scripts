4e39d07c46a5aab8b18c75fad562cee1f5bca793
pzim <52496172+pzim-devdata@users.noreply.github.com>
2023-09-07 12:47:13 +0200

    Update mpv-sub_not_forced_not_sdh.lua

--- mpv-sub_not_forced_not_sdh.lua
+++ mpv-sub_not_forced_not_sdh.lua
@@ -1,12 +1,12 @@
 function select_not_forced_subtitle()
     local last_sub = nil
     for i, sub in ipairs(mp.get_property_native("track-list")) do
-        if sub.type == "sub" and not sub.forced and (sub.title == nil or not sub.title:find("SDH")) then
+        if sub.type == "sub" and not sub.forced and (sub.title == nil or not sub.title:find("SDH") and not string.lower(sub.title):find("force")) then
             last_sub = sub
         end
     end
     if last_sub then
-        mp.set_property("sid", tostring(last_sub.id))
+        mp.set_property("sid", last_sub.id)
     end
 end
 mp.register_event("file-loaded", select_not_forced_subtitle)
